9e115fd74c3da43a339cbcba66a5c68341fed950
DESKTOP-801R80M\Rashid Saad <rashidsaad771@gmail.com>
2024-07-01 14:56:46 +0500

    react router

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -7,7 +7,8 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-import GitHub from './components/GitHub/GitHub.jsx'
+import GitHub, { githubInfoLoader } from './components/GitHub/GitHub.jsx'
+import User from './components/User/User.jsx'
 
 
 // const router = createBrowserRouter([
@@ -31,9 +32,14 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/github' element={<GitHub />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userId' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='/github' 
+      element={<GitHub />}
+       />
     </Route>
   )
 )
